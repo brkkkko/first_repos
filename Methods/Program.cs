6e63c41f9adb7681e8b0be50e741da64eaf02b8f
Alexey <a.chuvikin99@gmail.com>
2022-08-07 12:32:18 +0300

    Learned methods

---
+++ Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+int n = array.Length;
+int find = 4;
+
+int i = 0;
+
+while (i < n)
+{
+    if (array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}

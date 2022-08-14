9d1849e298b93a31a6a801e6d332fe0d1a3dc0bb
Alexey <a.chuvikin99@gmail.com>
2022-08-14 12:17:26 +0300

    Added For

---
+++ For/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+string Method4(int count, string c)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res = Method4(10, " Text of Method4 ");
+// Console.WriteLine(res);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
